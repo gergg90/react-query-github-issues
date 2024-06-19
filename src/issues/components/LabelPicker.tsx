f1a2473b16123dce880cca9aa4f188bb9b5d5eae
Germain <germain.gg90@gmail.com>
2024-06-19 18:54:32 -0400

    react query addred and label fetch

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+
+const getLabelsIssues = async () => {
+  const res = await fetch("https://github.com/facebook/react/issues");
+  const data = await res.json();
+  return data;
+};
 
 export const LabelPicker = () => {
+  const labelsQuery = useQuery({
+    queryKey: ["labels"],
+    queryFn: getLabelsIssues,
+  });
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
+      >
+        Primary
+      </span>
     </div>
-  )
-}
+  );
+};
