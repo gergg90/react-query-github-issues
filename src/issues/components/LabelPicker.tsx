386ed6f52d29ef6381575f2cba6235c9ccc6cb68
Germain <germain.gg90@gmail.com>
2024-06-20 21:36:32 -0400

    selected label

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,7 +1,15 @@
 import LoadingIcon from "../../shared/components/LoadingIcon";
 import { useLabels } from "../hooks/useLaberls";
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (label: string) => void;
+}
+
+export const LabelPicker = ({
+  selectedLabels: selectedLabels,
+  onChange,
+}: Props) => {
   const labelsQuery = useLabels();
 
   if (labelsQuery.isLoading) return <LoadingIcon />;
@@ -10,7 +18,10 @@ export const LabelPicker = () => {
       {labelsQuery.data?.map((label) => (
         <span
           key={label.id}
-          className="badge rounded-pill m-1 label-picker"
+          onClick={() => onChange(label.name)}
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "label-active" : ""
+          }`}
           style={{
             border: `1px solid #${label.color}`,
             color: `#${label.color}`,
