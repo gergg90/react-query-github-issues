ee2dd3f6ccb31f031224dd3ed3e9c5b6a42eff80
Germain <germain.gg90@gmail.com>
2024-06-26 12:12:53 -0400

    hook issue infinitescroll

--- src/issues/hooks/useIssuesInfiniteScroll.tsx
+++ src/issues/hooks/useIssuesInfiniteScroll.tsx
@@ -1,5 +1,54 @@
-function useIssuesInfiniteScroll() {
-  return <div>useIssuesInfiniteScroll</div>;
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { githubApi } from "../../apis/githubApi";
+import { sleep } from "../../helpers/sleep";
+import { Issue, State } from "../interfaces";
+
+interface Props {
+  state?: State;
+  labels: string[];
+  page?: number;
 }
 
-export default useIssuesInfiniteScroll;
+interface QueryProps {
+  pageParams?: number;
+  queryKey: (string | Props)[];
+}
+
+const getIssuesInfinite = async ({
+  pageParams = 1,
+  queryKey,
+}: QueryProps): Promise<Issue[]> => {
+  await sleep(2);
+
+  const [, , args] = queryKey;
+  const { state, labels } = args as Props;
+
+  const params = new URLSearchParams();
+
+  if (state) params.append("state", state);
+  if (labels.length > 0) {
+    const labelString = labels.join(",");
+    params.append("labels", labelString);
+  }
+
+  params.append("page", pageParams?.toString());
+
+  params.append("per_page", "5");
+
+  const { data } = await githubApi.get<Issue[]>("/issues", {
+    params,
+  });
+  return data;
+};
+
+export const useIssuesInfiniteScroll = ({ labels, state }: Props) => {
+  const issuesQueryInfinite = useInfiniteQuery({
+    queryKey: ["issues", "infinite", { labels, state, page: 1 }],
+    queryFn: (data) => getIssuesInfinite(data),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) =>
+      undefined,
+  });
+
+  return { issuesQueryInfinite };
+};
--- src/issues/views/ListViewInfiteScroll.tsx
+++ src/issues/views/ListViewInfiteScroll.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import LoadingIcon from "../../shared/components/LoadingIcon";
 import { IssueList } from "../components/IssueList";
 import { LabelPicker } from "../components/LabelPicker";
-import { useIssues } from "../hooks";
+import { useIssuesInfiniteScroll } from "../hooks";
 import { State } from "../interfaces";
 
 export const ListViewInfiniteScroll = () => {
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
 
   const [state, setState] = useState<State>();
-  const { issuesQuery, page, nextPage, prevPage } = useIssues({
+  const { issuesQueryInfinite } = useIssuesInfiniteScroll({
     state,
     labels: selectedLabels,
   });
@@ -23,18 +23,18 @@ export const ListViewInfiniteScroll = () => {
   return (
     <div className="row mt-5">
       <div className="col-8">
-        {issuesQuery.isLoading ? (
+        {issuesQueryInfinite.isLoading ? (
           <LoadingIcon />
         ) : (
           <IssueList
             state={state}
             onStateChange={(newState) => setState(newState)}
-            issues={issuesQuery.data || []}
+            issues={issuesQueryInfinite.data?.pages.flat() || []}
           />
         )}
 
         <div className="card-footer bg-dark d-flex justify-content-between align-items-center pt-2">
-          <button className="btn btn-primary">Load Data</button>
+          <button className="btn btn-primary">Load more...</button>
         </div>
       </div>
 
